d2f09247e8bf6e2ebe6925b702a62afd22067905
Alexander <Alex3296@yandex.ru>
2017-03-29 06:00:37 +0300

    Added empty project

--- JVM/Program.cs
+++ JVM/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _3_JVM {
+namespace JVM {
     class Program {
         static void Main(string[] args) {
         }
